8a18cb350abf9b9c9177ee976b22a392eb4b0c83
tsvetoslavpetkov <Tsvetoslav.andreev.petkov@gmail.com>
2021-11-25 20:00:18 +0200

    асдса

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom'
+import { Grid, Button } from '@mui/material';
+import './Nav.css';
 
 export default function Nav() {
     return (
         <div>
-            <Link to="/home">Home</Link>
+
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to="/home"><Button>Home</Button></Link>
+                    <Link to="/doctors"><Button>All Doctors</Button></Link>
+                    <Link to="/about"><Button>About</Button></Link>
+                </Grid>
+                <Grid item xs={3}>
+                <Link to="/home"><Button>Home</Button></Link>
+                    <Link to="/doctors"><Button>All Doctors</Button></Link>
+                    <Link to="/about"><Button>About</Button></Link>
+                </Grid>
+            </Grid>
+
+
+
         </div>
     )
 }
