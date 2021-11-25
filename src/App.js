1eb5fd6ba52cc9787b12ff86124300aab5257d77
tsvetoslavpetkov <Tsvetoslav.andreev.petkov@gmail.com>
2021-11-25 20:51:20 +0200

    set footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Nav.js';
+import Footer from './components/Footer/Footer.js'
 import Home from './components/home/Home.js';
 import About from './components/about/About.js'
 import Doctors from './components/doctors/Doctors.js';
@@ -14,8 +15,12 @@ export default function App() {
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Doctors} />
+        <Route path="/login" component={Doctors} />
+        <Route path="/register" component={Doctors} />
         <Route path="/" component={Home} />
     </main>
+
+    <Footer />
 
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function Footer() {
+    return (
+        <Container>
+            <Row>
+                <Col sm={8}>sm=8</Col>
+                <Col sm={4}>sm=4</Col>
+            </Row>
+            <Row>
+                <Col sm>sm=true</Col>
+                <Col sm>sm=true</Col>
+                <Col sm>sm=true</Col>
+            </Row>
+        </Container>
+    )
+}
