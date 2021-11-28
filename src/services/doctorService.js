77dba72981e90a1b21bd5959d637ba6e19e9da78
tsvetoslavpetkov <77385715+tsvetoslavpetkov@users.noreply.github.com>
2021-11-28 11:09:15 +0200

    fix npm errors

--- src/services/doctorService.js
+++
@@ -1,9 +0,0 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
-
-export async function getAllDoctors(db) {
-    const doctorsCol = collection(db, 'doctors');
-    const doctorsSnapshot = await getDocs(doctorsCol);
-    const doctorsList = doctorsSnapshot.docs.map(doc => doc.data());
-    console.log(doctorsList);
-    return doctorsList;
-  }
