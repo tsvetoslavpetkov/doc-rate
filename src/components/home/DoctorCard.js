7f2452d9666caa895b6e44ceca862fbfe039ce63
tsvetoslavpetkov <77385715+tsvetoslavpetkov@users.noreply.github.com>
2021-12-15 00:20:02 +0200

    remove space

--- src/components/home/DoctorCard.js
+++ src/components/home/DoctorCard.js
@@ -6,10 +6,9 @@ export default function DoctorCard({
     doctor
 }) {
     return (
-        <Card  border="light" className="home-card main-card" style={{ width: '15rem' }}>
+        <Card border="light" className="home-card main-card" style={{ width: '15rem' }}>
             <div className="home-card-img" variant="top" style={{ backgroundImage: `url(${doctor.imageUrl})` }}> </div>
             <Card.Body>
-
                 <h5 className="">{doctor.title} {doctor.firstName} {doctor.secondName}</h5>
                 <h6 className="text-secondary font-weight-light">{doctor.specialityName}</h6>
                 <Link to={'doc/' + doctor._id}><Button className="btn-sm" variant="primary">Детайли</Button></Link>
